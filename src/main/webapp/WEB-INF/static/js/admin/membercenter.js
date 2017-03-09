6628c942defb65acd5ed557a39380b615007bad7
liupengyan <liupengyanabc@163.com>
2017-03-09 13:42:53 +0800

    初始化会员中心

---
+++ src/main/webapp/WEB-INF/static/js/admin/membercenter.js
@@ -0,0 +1,133 @@
+/**
+ * Created by liupengyan on 17/3/9.
+ */
+
+require.config({
+    baseUrl : EK.STATIC_ROOT + "js" ,
+    shim:{
+        'lib/angular':{
+            exports:'angular'
+        },
+        'lib/angular-route':{
+            deps:['lib/angular'],
+            exports: 'angular-route'
+        },
+        'metaumeditor/lib/umeditor/dist/utf8-php/umeditor':{
+            deps:['lib/jquery']
+        },
+        'metaumeditor/lib/umeditor/dist/utf8-php/umeditor.config':{
+            deps:['metaumeditor/lib/umeditor/dist/utf8-php/umeditor']
+        },
+        'metaumeditor/src/meta.umeditor':{
+            deps:['lib/angular','lib/jquery']
+        }
+    }
+});
+requirejs(
+    [ 'lib/angular',
+        'controller/admin/menbercentController',
+        'lib/remoteUrl',
+        'dirctive/tipsDirctive',
+        'lib/requestParamUtill',
+        'dirctive/menuDirective',
+        'dirctive/fileUploadDirective',
+        'service/fileReaderService',
+        'lib/angular-route',
+        'dirctive/ng-pagination',
+        'lib/jquery',
+        'metaumeditor/lib/umeditor/dist/utf8-php/umeditor',
+        'metaumeditor/lib/umeditor/dist/utf8-php/umeditor.config',
+        'metaumeditor/src/meta.umeditor'
+    ],
+    function(angular,menbercentController,remoteUrl) {
+
+        var app = angular.module("menbercent", ['ngRoute','tips','httphelper','menu','fileuploadModel','fileReaderModel','ng-pagination','meta.umeditor']);
+
+//路由配置
+        app.config(["$routeProvider", function($routeProvider) {
+            $routeProvider.when("/memberSet", {
+                templateUrl: "/static/temp/admin/membercenter/memberSet.html"
+
+            }).when("/levelSet", {//会员级别及权益设置
+                templateUrl: "/static/temp/admin/membercenter/levelSet.html"
+
+            }).when("/regulations", {//会员章程展示
+                templateUrl: "/static/temp/admin/membercenter/regulations.html",
+                contoller:menbercentController.regulationsController
+
+            }).when("/editRegulations", {//会员章程修改
+                templateUrl: "/static/temp/admin/membercenter/editRegulations.html"
+
+            }).when("/expandinfo", {//会员拓展资料
+                templateUrl: "/static/temp/admin/membercenter/expandinfo.html"
+
+
+            }).otherwise({
+                redirectTo: "/index"
+            });
+
+        }]);
+
+
+        app.controller('menbercentCtrl',['$scope','$http',function($scope,$http){
+            $scope.config = {};
+            //初始化菜单数据
+            $scope.menuData=[
+                {id:'memberSet',link: '#!/memberSet', name: "会员管理",hasnext:false,next:[]},
+                {id:'levelSet',link: '#!/levelSet', name: "会员级别及权益设置",hasnext:false,next:[]},
+                {id:'regulations',link: '#!/regulations', name: "会员章程设置",hasnext:false, next:[]},
+                {id:'expandinfo',link: '#!/expandinfo', name: "会员拓展资料",hasnext:false,next:[ ]}
+            ];
+
+            //初始化用户信息
+            $http.post(remoteUrl.getLoginUserInfo).then(
+                function(result){
+
+                    var rs =result.data;
+                    var code =rs.code;
+                    var data=rs.data;
+                    if(code==1){
+                        $scope.strRealname=data.strRealname;
+                        var strMerchantname= data.strMerchantname;
+                        if(strMerchantname==""){
+                            strMerchantname="4s名称"
+                        }
+                        $scope.strMerchantname=strMerchantname;
+
+                        if( !data.strMerchantlogo){
+                            $scope.had4sImage=false;
+                        }else{
+
+                            $scope.had4sImage=true;
+                            $scope.strMerchantlogo=data.strMerchantlogo
+                            $scope.strImgrootpath=data.strImgrootpath;
+                        }
+
+                    }else if(code ==-1){
+                        //得到登录路径
+                        window.location.href="/admin/login?url="+window.location.pathname+window.location.search+window.location.hash;
+                        //未登录
+                    }else if(code<=-2&&code>=-7){
+                        //必填字段未填写
+                        $scope.showAlert(data.msg);
+                    }else if(code ==-8){
+                        //暂无数据
+                    }
+
+                },
+                function(result){
+                    var status=result.status;
+                    if(status==-1){
+                        $scope.showAlert("服务器错误")
+                    }else if(status>=404&&status<500){
+                        $scope.showAlert("请求路径错误")
+                    }else if(status>=500){
+                        $scope.showAlert("服务器错误")
+                    }
+                }
+
+            );
+        }]);
+
+        angular.bootstrap(document, ["menbercent"]);
+    });
--- src/main/webapp/WEB-INF/views/admin/membercenter.jsp
+++ src/main/webapp/WEB-INF/views/admin/membercenter.jsp
@@ -1,15 +1,78 @@
-<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
-<% String basePath = request.getContextPath();%>
-<!DOCTYPE html>
-<jsp:include page="common.jsp"/>
-<html>
-<head>
-<link href="<%=basePath %>/static/css/index/index.css" rel="stylesheet" type="text/css"/>
-<script type="text/javascript" src="<%=basePath %>/static/js/index/index.js"></script>
-<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-<title>会员中心</title>
-</head>
-<body>
-	会员中心
-</body>
-</html>
+    <%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8" %>
+            <% String basePath = request.getContextPath();%>
+
+        <!DOCTYPE html>
+        <html >
+        <script>
+        function logoImgError(_this)
+        {
+        _this.src="/static/images/logo.png";
+        };
+
+        </script>
+        <head>
+        <%@ include file="../version.jsp" %>
+        <script type="text/javascript">
+
+        (function(){
+
+        var css ='<link type="text/css" rel="stylesheet" href="'+ EK.STATIC_ROOT+'css/libs/base.css">'
+        +'<link type="text/css" rel="stylesheet" href="'+ EK.STATIC_ROOT+'css/libs/global.css">'
+        +'<link type="text/css" rel="stylesheet" href="'+ EK.STATIC_ROOT+'css/pages/admin/global.css">'
+        +'<link type="text/css" rel="stylesheet" href="'+ EK.STATIC_ROOT+'css/dirctive/leftmenu.css">'
+        +'<link type="text/css" rel="stylesheet" href="'+ EK.STATIC_ROOT+'css/dirctive/ng-pagination.css">'
+        +'<link type="text/css" rel="stylesheet" href="'+ EK.STATIC_ROOT+'js/metaumeditor/lib/umeditor/dist/utf8-php/themes/default/css/umeditor.css">'
+        +'<link type="text/css" rel="stylesheet" href="'+ EK.STATIC_ROOT+'css/pages/admin/homepage.css">'
+        document.write(css);
+        })();
+        </script>
+
+
+        <%--  <script type="text/javascript" src="<%=basePath %>/static/js/lib/angular-route.js"></script>--%>
+
+
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <meta name="renderer" content="webkit">
+        <meta name="applicable-device" content="pc">
+        <meta http-equiv="X-UA-Compatible" content="IE=Edge">
+        <meta http-equiv="Cache-Control" content="no-transform">
+        <meta name="application-name" content="">
+        <meta name="description" content="E-card后台管理">
+
+        <title>E-card后台管理</title>
+        </head>
+        <body ng-controller="menbercentCtrl" >
+        <input type="hidden" ng-init="membercenter=true" ng-model="membercenter"/>
+        <alert></alert>
+        <comfirm></comfirm>
+        <toast></toast>
+
+
+        <div ng-include="'/static/temp/admin/header.html'"></div>
+
+        <div class="-lpy-main-content " >
+        <div class="left-menu fl" >
+        <menu></menu>
+        </div>
+
+        <div class="right-content fl" >
+        <div ng-view></div>
+        </div>
+        <div class="clearfix"></div>
+        </div>
+
+
+        <div ng-include="'/static/temp/admin/footer.html'"></div>
+        </body>
+        </html>
+
+        <script type="text/javascript">
+        (function(){
+        var footer = '<script src="'+ EK.STATIC_ROOT+'js/lib/require.js" data-main="'+
+        EK.STATIC_ROOT+'js/admin/membercenter"><\/script>';
+        document.write(footer);
+        })();
+
+        </script>
+
+
