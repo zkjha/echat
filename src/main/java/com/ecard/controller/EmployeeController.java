0c06b31ed9de56ece02f5216caa1418d4d39be90
Administrator <Administrator@dhx>
2017-03-09 10:03:43 +0800

    手机号格式校验

--- src/main/java/com/ecard/controller/EmployeeController.java
+++ src/main/java/com/ecard/controller/EmployeeController.java
@@ -89,10 +89,10 @@ public class EmployeeController {
 		if(ValidateTool.isEmptyStr(strDutyid)) {
 			return DataTool.constructResponse(ResultCode.CAN_NOT_NULL, "职务ID不能为空", null);
 		}
-		if(!ValidateTool.isEmptyStr(strMobile)) {
+		if(ValidateTool.isEmptyStr(strMobile)) {
 			return DataTool.constructResponse(ResultCode.CAN_NOT_NULL, "电话不能为空", null);
 		}
-		if(ValidateTool.isMobile(strMobile)) {
+		if(!ValidateTool.isMobile(strMobile)) {
 			return DataTool.constructResponse(ResultCode.MOBILE_FORMAT_ERROR, "电话格式错误", null);
 		}
 		if(ValidateTool.isEmptyStr(strRealname)) {
@@ -191,10 +191,10 @@ public class EmployeeController {
 		if(ValidateTool.isEmptyStr(strDutyid)) {
 			return DataTool.constructResponse(ResultCode.CAN_NOT_NULL, "职务ID不能为空", null);
 		}
-		if(!ValidateTool.isEmptyStr(strMobile)) {
+		if(ValidateTool.isEmptyStr(strMobile)) {
 			return DataTool.constructResponse(ResultCode.CAN_NOT_NULL, "电话不能为空", null);
 		}
-		if(ValidateTool.isMobile(strMobile)) {
+		if(!ValidateTool.isMobile(strMobile)) {
 			return DataTool.constructResponse(ResultCode.MOBILE_FORMAT_ERROR, "电话格式错误", null);
 		}
 		if(ValidateTool.isEmptyStr(strRealname)) {
