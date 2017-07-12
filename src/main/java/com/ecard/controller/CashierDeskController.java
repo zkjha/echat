761ce9cf7e1e2b51e2b525e988d2173f51677284
李顺红 <185730069@qq.com>
2017-07-12 09:04:53 +0800

    test

--- src/main/java/com/ecard/controller/CashierDeskController.java
+++ src/main/java/com/ecard/controller/CashierDeskController.java
@@ -19,7 +19,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-/*
+
 import org.apache.commons.lang.StringUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
@@ -35,7 +35,7 @@ import com.alipay.demo.trade.service.AlipayTradeService;
 import com.alipay.demo.trade.service.impl.AlipayMonitorServiceImpl;
 import com.alipay.demo.trade.service.impl.AlipayTradeServiceImpl;
 import com.alipay.demo.trade.service.impl.AlipayTradeWithHBServiceImpl;
-*/
+
 import com.commontools.data.DataTool;
 import com.commontools.date.DateStyle;
 import com.commontools.date.DateTool;
@@ -63,37 +63,36 @@ public class CashierDeskController
 	@Autowired
 	private WebSessionUtil webSessionUtil;
 	
-	//@Autowired
-	//private static AlipayTradeService   tradeService;
+//	@Autowired
+//	private static AlipayTradeService   tradeService;
 	//@Autowired
     // 支付宝当面付2.0服务（集成了交易保障接口逻辑）
-   // private static AlipayTradeService   tradeWithHBService;
+//    private static AlipayTradeService   tradeWithHBService;
 	//@Autowired
     // 支付宝交易保障接口服务，供测试接口api使用，请先阅读readme.txt
-	//private static AlipayMonitorService monitorService;
-	/*
-	private static Log                  log = LogFactory.getLog(Main.class);
-	 static {
+//	private static AlipayMonitorService monitorService;
+	//@Autowired
+//	private static Log                  log = LogFactory.getLog(Main.class);
+//	 static {
 	
 	        /** 一定要在创建AlipayTradeService之前调用Configs.init()设置默认参数
 	         *  Configs会读取classpath下的zfbinfo.properties文件配置信息，如果找不到该文件则确认该文件是否在classpath目录
 	         */
-		 
-	    //   Configs.init("zfbinfo.properties");
+		// 	       Configs.init("zfbinfo.properties");
 
 	        /** 使用Configs提供的默认参数
 	         *  AlipayTradeService可以使用单例或者为静态成员对象，不需要反复new
 	         */
-	     //  tradeService = new AlipayTradeServiceImpl.ClientBuilder().build();
+	     // tradeService = new AlipayTradeServiceImpl.ClientBuilder().build();
 
 	        // 支付宝当面付2.0服务（集成了交易保障接口逻辑）
-	   //   tradeWithHBService = new AlipayTradeWithHBServiceImpl.ClientBuilder().build();
+	  //    tradeWithHBService = new AlipayTradeWithHBServiceImpl.ClientBuilder().build();
 	     
 	        /** 如果需要在程序中覆盖Configs提供的默认参数, 可以使用ClientBuilder类的setXXX方法修改默认参数 否则使用代码中的默认设置 */
-	    //    monitorService = new AlipayMonitorServiceImpl.ClientBuilder()
-	     //       .setGatewayUrl("http://mcloudmonitor.com/gateway.do").setCharset("GBK")
+	   //     monitorService = new AlipayMonitorServiceImpl.ClientBuilder()
+	    //        .setGatewayUrl("http://mcloudmonitor.com/gateway.do").setCharset("GBK")
 	     //       .setFormat("json").build();
-	 //  }
+//	   }
 	
 	//根据手机号或会员卡号、姓名搜索会员信息
 	@ResponseBody
@@ -678,8 +677,8 @@ public class CashierDeskController
 	
 	
 	//支付宝支付
-	//@ResponseBody
-	//@RequestMapping("payWithAliPayment")
+//	@ResponseBody
+//	@RequestMapping("payWithAliPayment")
 	//localhost:8083/admin/biz/CashierDesk/payWithAliPayment?strOrderId=oeojgjg
 /*
 	public String payWithAliPayment(HttpServletRequest request, HttpServletResponse response)
@@ -815,8 +814,7 @@ public class CashierDeskController
     }		
 	
 
-	*/
-
+*/
 	// 简单打印应答
 /*
 	private void dumpResponse(AlipayResponse response) {
